77fbd2142b1474826a71488d91bbf3b897fce963
dahmie <omotayodamilare07@gmail.com>
2026-04-04 03:31:51 +0100

    chore: update database engine

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -8,3 +8,19 @@ from dotenv import load_dotenv  # type: ignore
 
 load_dotenv()
 DB_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DB_URL  # type: ignore
+                       , connect_args={
+                           "check_same_thread": False})
+
+session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
