b5a8472b8d09eeaa5d73d85eedfb36058a265fad
dahmie <omotayodamilare07@gmail.com>
2026-04-04 03:43:48 +0100

    feat: add routes and routers

--- routes.py
+++ routes.py
@@ -1,3 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from database import Base
+from sqlalchemy.orm import Session
+from typing import List
+
+import services
 
 router = APIRouter()
+
+
+class ContactMessage(BaseModel):
+    name: str
+    email: str
+    content: str
+
+
+class PostCreate(BaseModel):
+    title: str
+    topic: str
+    date: str
+    tags: List[str]
+    content: str
+
+
+class PostUpdate(BaseModel):
+    title: str
+    topic: str
+    tags: List[str]
+    content: str
