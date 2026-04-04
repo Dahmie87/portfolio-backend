a28740ef7ffc2f80cbf54c5dd7ff6d83ebee479b
dahmie <omotayodamilare07@gmail.com>
2026-04-04 11:54:05 +0100

    chore: update main py routes

--- routes.py
+++ routes.py
@@ -1,21 +1,21 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from database import Base
+from database import get_db
 from sqlalchemy.orm import Session
 from typing import List
 
 import services
 
-router = APIRouter()
-
 
 class ContactMessage(BaseModel):
+    id: int
     name: str
     email: str
     content: str
 
 
 class PostCreate(BaseModel):
+    id: int
     title: str
     topic: str
     date: str
@@ -27,3 +27,45 @@ class PostUpdate(BaseModel):
     title: str = None  # type: ignore
     tags: List[str] = None  # type: ignore
     content: str = None  # type: ignore
+
+
+router = APIRouter()
+
+
+@router.post("/api/contact")
+def submit_contact(data: ContactMessage, db: Session = Depends(get_db)):
+    return {
+        "id": data.id,
+        "name": data.name,
+        "status": "message srnt succesfully"
+    }
+
+
+@router.get('/api/list_contacts')
+def list_contact(db: Session = Depends(get_db)):
+    pass
+
+
+@router.post("/api/post")
+def create_new_post(post: PostCreate):
+    pass
+
+
+@router.get('/api/posts')
+def list_all_posts(db: Session = Depends(get_db)):
+    pass
+
+
+@router.get('/api/posts{id}')
+def find_post(iid: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.put('/api/posts/{id}')
+def update_post(id: int, post: PostUpdate, db: Session = Depends(get_db)):
+    pass
+
+
+@router.delete('/api/posts/{id}')
+def delete_post(id: int, db: Session = Depends(get_db)):
+    pass
