8a5854fe5f86a8536c61016aff03df4c2ef4a23c
dahmie <omotayodamilare07@gmail.com>
2026-04-04 03:01:10 +0100

    test: index route

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
 from routes import router
 # from fastapi
+
+app = FastAPI(title="Portfolio 3D API", version="1.0.0")
+app.include_router(router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Portfolio 3D API!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
